c0a0d79ac3317c71313ea84e562c14d052c3d271
anonymous243 <anonymous24tr@gmail.com>
2025-05-06 17:20:14 +0530

    Updated files for GitHub Pages deployment

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./client/src/App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
